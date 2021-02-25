f05b4edd50735bf16ad40b20bf0b88c1388f8e85
Paulo Salvatore <salvatoregames@gmail.com>
2021-02-25 04:06:11 -0300

    APK build

--- Builds/unityLibrary/src/main/java/com/unity3d/player/CustomReceiver.java
+++ Builds/unityLibrary/src/main/java/com/unity3d/player/CustomReceiver.java
@@ -3,10 +3,13 @@ package com.unity3d.player;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class CustomReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Intent i = new Intent(context, UnityPlayerActivity.class);
+
+        Log.d("CUSTOM_RECEIVER", i.toString());
 
         if (i != null) {
             context.startActivity(i);
